c6e44e56027aae7b8b131619c720e0fa42d34144
sardor <sardorsaydaxmatov14@gmail.com>
2023-03-21 15:16:15 +0500

    new events

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ const shortBreak = document.querySelector(".shortBreak");
 const longBreak = document.querySelector(".longBreak");
 const pomodoro = document.querySelector(".pomodoro");
 
+let time = 1500;
+let intervalID;
+
+shortBreak.addEventListener('click', function(){
+  document.body.style.background = 'blue';
+  document.querySelector('.main').style.background = "darkblue";
+  startBtn.style.color = "blue";
+})
+
+longBreak.addEventListener('click', function(){
+  document.body.style.background = 'yellow';
+  document.querySelector('.main').style.background = "rgb(192, 190, 32)";
+  startBtn.style.color = "yellow";
+})
+
+pomodoro.addEventListener('click', function(){
+  document.body.style.background = 'rgb(186, 73, 73)';
+  document.querySelector('.main').style.background = "#c15c5c";
+  startBtn.style.color = "rgb(186, 73, 73)";
+})
