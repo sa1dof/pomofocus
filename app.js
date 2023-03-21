253bd56a716b238dfbba66cb187b6296012cf5d3
sa1doff <murodilxonsaidov22@gmail.com>
2023-03-21 14:58:37 +0500

    delete events code

--- app.js
+++ app.js
@@ -6,21 +6,3 @@ const shortBreak = document.querySelector(".shortBreak");
 const longBreak = document.querySelector(".longBreak");
 const pomodoro = document.querySelector(".pomodoro");
 
-pomodoro.addEventListener("click", () => {
-  pomodoro.style.backgroundColor = "#ff8a8a";
-  document.body.style.backgroundColor = "#ff4b4b";
-  timer.textContent = "25:00";
-});
-
-longBreak.addEventListener("click", () => {
-  pomodoro.style.backgroundColor = "#406b8f";
-  document.body.style.backgroundColor = "#225680";
-  timer.innerHTML = "15:00";
-});
-
-shortBreak.addEventListener("click", () => {
-  pomodoro.style.backgroundColor = "#89c2f0";
-  document.body.style.backgroundColor = "#5faeee";
-  timer.innerHTML = "05:00";
-});
-console.log("hghhhf");
