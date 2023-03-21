b8a42166c027a437a0c37a2a23a30a67a3e2f831
Fayzulloh <raxmatovfayzullo68@gmail.com>
2023-03-21 14:41:38 +0500

    short event

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 const timer = document.querySelector(".main-time");
 const actionBtns = document.querySelectorAll(".main-menu-item");
 const startBtn = document.getElementById("startBtn");
-const shortBreak = document.querySelector('.shortBreak');
-const longBreak = document.querySelector('.longBreak')
-const pomodoro = document.querySelector('.pomodoro')
+const shortBreak = document.querySelector(".shortBreak");
+const longBreak = document.querySelector(".longBreak");
+const pomodoro = document.querySelector(".pomodoro");
+
+shortBreak.addEventListener("click", function () {
+  document.body.style.background = "blue";
+  document.querySelector(".main").style.background = "darkblue";
+});
