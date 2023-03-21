0a4cf7b973cb5d2d7d1c618086ba6773a6981188
sa1doff <murodilxonsaidov22@gmail.com>
2023-03-21 14:34:34 +0500

    html css

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+const timer = document.querySelector(".main-time");
+const actionBtns = document.querySelectorAll(".main-menu-item");
+const startBtn = document.getElementById("startBtn");
+const shortBreak = document.querySelector('.shortBreak');
+const longBreak = document.querySelector('.longBreak')
+const pomodoro = document.querySelector('.pomodoro')
