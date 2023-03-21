faf62ddcfb1d2911001ec51020af172bd7df5d2d
Fayzulloh <raxmatovfayzullo68@gmail.com>
2023-03-21 14:48:19 +0500

    add enents

--- app.js
+++ app.js
@@ -6,7 +6,20 @@ const shortBreak = document.querySelector(".shortBreak");
 const longBreak = document.querySelector(".longBreak");
 const pomodoro = document.querySelector(".pomodoro");
 
-shortBreak.addEventListener("click", function () {
-  document.body.style.background = "blue";
-  document.querySelector(".main").style.background = "darkblue";
+pomodoro.addEventListener("click", () => {
+  pomodoro.style.backgroundColor = "#ff8a8a";
+  document.body.style.backgroundColor = "#ff4b4b";
+  timer.textContent = "25:00";
+});
+
+longBreak.addEventListener("click", () => {
+  pomodoro.style.backgroundColor = "#406b8f";
+  document.body.style.backgroundColor = "#225680";
+  timer.innerHTML = "15:00";
+});
+
+shortBreak.addEventListener("click", () => {
+  pomodoro.style.backgroundColor = "#89c2f0";
+  document.body.style.backgroundColor = "#5faeee";
+  timer.innerHTML = "05:00";
 });
